70d6179c0bbcf6db9bf50ebfc09545f5ff07437b
Robert Ritchi Alves da Silva <ritchierobert2017@gmail.com>
2026-05-18 19:36:09 -0300

    Terminei a classe UsuarioController

--- backend/src/main/java/com/bwartsmaker/backend/controller/UsuarioController.java
+++ backend/src/main/java/com/bwartsmaker/backend/controller/UsuarioController.java
@@ -7,3 +7,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<UsuarioEntity> getAllUsuarios() {
+        return usuarioService.listarTodosUsuarios();    
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioEntity getUsuarioById(@PathVariable Long id) {
+        return usuarioService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public UsuarioEntity createUsuario(@RequestBody UsuarioEntity usuario) {
+        return usuarioService.criarUsuario(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public UsuarioEntity updateUsuario(@PathVariable Long id, @RequestBody UsuarioEntity usuario) {
+        return usuarioService.atualizarUsuario(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable Long id) {
+        usuarioService.deletarUsuario(id);;
+    }
+}
