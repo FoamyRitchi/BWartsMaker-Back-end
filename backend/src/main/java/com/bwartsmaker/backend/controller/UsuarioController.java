5ad31131575cc9bbd535979842444cdebb707692
FoamyRitchi <101297780+FoamyRitchi@users.noreply.github.com>
2026-05-26 10:54:26 -0300

    FIX: arrumei uma parte do controller

--- backend/src/main/java/com/bwartsmaker/backend/controller/UsuarioController.java
+++ backend/src/main/java/com/bwartsmaker/backend/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.bwartsmaker.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bwartsmaker.backend.entity.UsuarioEntity;
 import com.bwartsmaker.backend.service.UsuarioService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
