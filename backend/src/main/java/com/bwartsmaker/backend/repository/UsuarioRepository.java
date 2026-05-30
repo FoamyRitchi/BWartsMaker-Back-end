4c0a7855349a044c9b536594af9e0567b6afcf12
Robert Ritchi Alves da Silva <ritchierobert2017@gmail.com>
2026-05-30 18:01:42 -0300

    Fix: arrumei o erro de autenticação

--- backend/src/main/java/com/bwartsmaker/backend/repository/UsuarioRepository.java
+++ backend/src/main/java/com/bwartsmaker/backend/repository/UsuarioRepository.java
@@ -5,11 +5,21 @@ import com.bwartsmaker.backend.entity.UsuarioEntity;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository <UsuarioEntity, Long> {
-    Optional<UsuarioEntity> findByEmail_userAndSenha_user(
-            String email_user,
-            String senha_user);
+     @Query("""
+        SELECT u
+        FROM UsuarioEntity u
+        WHERE u.email_user = :email
+        AND u.senha_user = :senha
+    """)
+    
+    Optional<UsuarioEntity> autenticar(
+        @Param("email") String email,
+        @Param("senha") String senha
+    );
 }
